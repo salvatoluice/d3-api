98475006e7a9e35da7522e25d201ef95ca8d0644
salvatoluice <salvatoluice5@gmail.com>
2024-04-12 16:07:43 +0300

    authorize requests

--- routes/voucherRoutes.js
+++ routes/voucherRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const voucherController = require('../controllers/VoucherController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/generate', voucherController.generateVoucher);
-router.get('/', voucherController.getAllVouchers);
-router.get('/check/:voucherCode', voucherController.checkVoucher);
+router.get('/', authMiddleware(['admin']), voucherController.getAllVouchers);
+router.get('/check/:voucherCode', authMiddleware(['seller', 'admin']), voucherController.checkVoucher);
 
 module.exports = router;
