97fb71455e7176045dacf1f0b2a366f47870e4f1
salvatoluice <salvatoluice5@gmail.com>
2024-04-15 19:35:02 +0300

    all users

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,6 +89,17 @@ exports.getCurrentUser = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
 exports.logout = async (req, res) => {
     res.status(200).json({ message: 'Logout successful' });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,3 @@
-// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
@@ -7,6 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+router.get('/', authController.getAllUsers);
 router.get('/current-user', authMiddleware(['user', 'seller', 'admin']), authController.getCurrentUser);
 
 module.exports = router;
