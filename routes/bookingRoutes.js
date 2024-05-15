e43d1148e8f7648c538a15ca22cd393fcc21b8db
salvatoluice <salvatoluice5@gmail.com>
2024-05-15 13:15:28 +0300

    middleware fix

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', bookingController.createBooking);
 router.get('/user/get', authMiddleware, bookingController.getUserBookings);
-router.get('/', authMiddleware, bookingController.getAllBookings);
+router.get('/', authMiddleware(['user', 'seller', 'admin']), bookingController.getAllBookings);
 
 module.exports = router;
